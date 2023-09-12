d19f20db5ffe180a26ca307ef08ebb664f6e48cc
Chayut Yuttapornpong <yoshi9968@gmail.com>
2023-09-12 18:27:15 +0700

    Google Map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,17 +12,22 @@ class map extends StatefulWidget {
 
 class _mapState extends State<map> {
   GoogleMapController? mapController;
-
+  final Completer<GoogleMapController> _controller = Completer();
   final LatLng _center = LatLng(13.812534711489294, 100.5116944);
 
   void _onMapCreated(GoogleMapController controller){
     mapController = controller;
   }
+  Marker _marker = Marker(
+      markerId: MarkerId('Marker1'),
+      position : LatLng(13.812534711489294, 100.5116944));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         onMapCreated: _onMapCreated,
+        markers: {_marker},
         initialCameraPosition: CameraPosition(target: _center,zoom: 11.0),
       ),
     );
