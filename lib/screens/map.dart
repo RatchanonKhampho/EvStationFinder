40d45a043e3798db64c3d1b48b0ed43e5829c7ee
Chayut Yuttapornpong <yoshi9968@gmail.com>
2023-09-25 18:33:09 +0700

    Google Map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:bottom_sheet/bottom_sheet.dart';
 class map extends StatefulWidget {
   const map({super.key});
 
@@ -10,32 +12,111 @@ class map extends StatefulWidget {
 }
 
 class _mapState extends State<map> {
-  GoogleMapController? _controller;
-  Set<Marker> _markers = Set<Marker>();
-
+  TextEditingController textController = TextEditingController();
+  GoogleMapController? _mapController;
+  // ตั้งค่าพิกัดเริ่มต้น
+  static const LatLng _initialCameraPosition = const LatLng(13.7563, 100.5018);
+  List<Marker> _markers = [];
+  bool _bottomSheetVisible = false;
+  String _selectedMarkerId = "";
+  var _currentIndex = 0;
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(13.812534711489294,100.5116944), // ตำแหน่งเริ่มต้นของแผนที่
-            zoom: 15.0, // ขนาดภาพ
-          ),
-          onMapCreated: (controller) {
-            setState(() {
-              _controller = controller;
-            });
-            _loadMarkersFromFirestore();
-          },
-          markers: _markers,
-        ),
-      ],
-    );
-    /*bottomNavigationBar: SalomonBottomBar(
+  void initState() {
+    super.initState();
+    _loadMarkersFromFirestore();
+  }
+    // ดึงข้อมูลหมุดจาก Firestore และสร้าง Marker
+    Future<void> _loadMarkersFromFirestore() async {
+      final markers = await FirebaseFirestore.instance.collection('locations')
+          .get();
+      setState(() {
+        _markers = markers.docs.map((doc) {
+          final data = doc.data() as Map<String, dynamic>;
+          final LatLng position = LatLng(data['latitude'], data['longitude']);
+          return Marker(
+            markerId: MarkerId(doc.id),
+            position: position,
+            onTap: () => _showMarkerDetails(doc.id),
+          );
+        }).toList();
+      });
+    }
+    // แสดงข้อมูลของหมุดผ่าน BottomSheet
+    void _showMarkerDetails(String markerId) {
+      showModalBottomSheet(
+shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(10),
+),
+        context: context,
+        builder: (context) {
+          // ดึงข้อมูลจาก Firestore โดยใช้ markerId
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance.collection('locations').doc(
+                markerId).get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('เกิดข้อผิดพลาดในการดึงข้อมูล');
+              }
+              final data = snapshot.data!.data() as Map<String, dynamic>;
+              // สร้าง UI สำหรับแสดงข้อมูล
+              return ListView(
+                  children :[
+                    Container(
+                  width: MediaQuery.of(context).size.width-30,
+                  height: MediaQuery.of(context).size.height-30 ,
+                  child: Column(
+
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text('Detail',textAlign: TextAlign.center,   style: TextStyle(
+                        fontSize: 20 ),),
+                      ),
+                      Image.network('${data['images']}'),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+
+                          title: Text('ชื่อ: ${data['name']}',style: TextStyle(
+                         fontSize: 20 ),),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0,left: 8.0),
+                        child: ListTile(
+                          title: Text('ที่อยู่: ${data['address']}'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0,left: 8.0),
+                        child: ListTile(
+                          title: Text('เวลา: ${data['time']}'),
+                        ),
+                      ),
+
+                      // เพิ่มข้อมูลอื่น ๆ ตามที่ต้องการ
+                    ],
+                  ),
+                ),
+              ],
+              );
+            },
+          );
+        },
+      );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        bottomNavigationBar: SalomonBottomBar(
           currentIndex: _currentIndex,
           onTap: (i) => setState(() => _currentIndex = i),
           items: [
+
             /// Home
             SalomonBottomBarItem(
               icon: Icon(Icons.search),
@@ -64,36 +145,69 @@ class _mapState extends State<map> {
               selectedColor: Colors.teal,
             ),
           ],
-        ));*/
-  }
-
-// โหลดข้อมูลจาก Firestore และสร้างหมุดบนแผนที่
-  void _loadMarkersFromFirestore() async {
-    final firestore = FirebaseFirestore.instance;
-    final snapshot = await firestore.collection('locations').get();
-
-    for (final doc in snapshot.docs) {
-      final data = doc.data() as Map<String, dynamic>;
-      final lat = data['latitude'] as double;
-      final lng = data['longitude'] as double;
-      final marker = Marker(
-        markerId: MarkerId(doc.id),
-        position: LatLng(lat, lng),
-        infoWindow: InfoWindow(
-          title: data['name'] ?? '',
-          snippet: data['description'] ?? '',
+        ),
+        body: SafeArea(
+          child: Stack(children: [
+          GoogleMap(
+          initialCameraPosition: CameraPosition(
+          target: _initialCameraPosition,
+            zoom: 14,
+          ),
+          onMapCreated: (controller) {
+            setState(() {
+              _mapController = controller;
+            });
+          },
+          markers: Set<Marker>.from(_markers),
+        ),
+            if (_bottomSheetVisible)
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.white,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('ข้อมูลจาก Firestore'),
+                      // ใส่โค้ดเพื่อแสดงข้อมูลจาก Firestore ของ _selectedMarkerId ที่เลือก
+                    ],
+                  ),
+                ),
+              ),
+            Column(
+              children: [
+                Positioned(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 10.0, left: 20, right: 20),
+                      child: seachBar(),
+                    )
+                ),
+              ],
+            ),
+          ]),
         ),
       );
-
-      setState(() {
-        _markers.add(marker);
-      });
     }
+
+
+    Widget seachBar() {
+      return AnimSearchBar(
+        width: 400,
+        textController: textController,
+        onSuffixTap: () {
+          setState(() {
+            textController.clear();
+          });
+        }, onSubmitted: (String) {
+
+      },
+      );
+    }
+
   }
 
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
-}
+
+
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database {
-
+  Future<List<Map<String, dynamic>>> fetchData() async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('location').get();
+    return querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+  }
   }
 
