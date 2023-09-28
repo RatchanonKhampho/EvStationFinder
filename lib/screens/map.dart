4f141b5e3fdd7adf5ea03346de7655c27c3dfe37
Chayut Yuttapornpong <yoshi9968@gmail.com>
2023-09-28 18:31:55 +0700

    update

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -4,6 +4,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:bottom_sheet/bottom_sheet.dart';
+
 class map extends StatefulWidget {
   const map({super.key});
 
@@ -12,239 +13,177 @@ class map extends StatefulWidget {
 }
 
 class _mapState extends State<map> {
-  final searchController = TextEditingController();
-
+  final TextEditingController _searchController = TextEditingController();
   GoogleMapController? _mapController;
   List<DocumentSnapshot> _searchResults = [];
+
   // ตั้งค่าพิกัดเริ่มต้น
   static const LatLng _initialCameraPosition = const LatLng(13.7563, 100.5018);
   List<Marker> _markers = [];
   bool _bottomSheetVisible = false;
   String _selectedMarkerId = "";
   var _currentIndex = 0;
+
   @override
   void initState() {
     super.initState();
     _loadMarkersFromFirestore();
-    _searchLocation('');
   }
-    // ดึงข้อมูลหมุดจาก Firestore และสร้าง Marker
-    Future<void> _loadMarkersFromFirestore() async {
-      final markers = await FirebaseFirestore.instance.collection('locations')
-          .get();
-      setState(() {
-        _markers = markers.docs.map((doc) {
-          final data = doc.data() as Map<String, dynamic>;
-          final LatLng position = LatLng(data['latitude'], data['longitude']);
-          return Marker(
-            markerId: MarkerId(doc.id),
-            position: position,
-            onTap: () => _showMarkerDetails(doc.id),
-          );
-        }).toList();
-      });
-    }
-    // แสดงข้อมูลของหมุดผ่าน BottomSheet
-    void _showMarkerDetails(String markerId) {
-      showModalBottomSheet(
-shape: RoundedRectangleBorder(
-  borderRadius: BorderRadius.circular(10),
-),
-        context: context,
-        builder: (context) {
-          // ดึงข้อมูลจาก Firestore โดยใช้ markerId
-          return FutureBuilder<DocumentSnapshot>(
-            future: FirebaseFirestore.instance.collection('locations').doc(
-                markerId).get(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return Text('เกิดข้อผิดพลาดในการดึงข้อมูล');
-              }
-              final data = snapshot.data!.data() as Map<String, dynamic>;
-              // สร้าง UI สำหรับแสดงข้อมูล
-              return ListView(
-                  children :[
-                    Container(
-                  width: MediaQuery.of(context).size.width-30,
-                  height: MediaQuery.of(context).size.height-30 ,
-                  child: Column(
 
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text('Detail',textAlign: TextAlign.center,   style: TextStyle(
-                        fontSize: 20 ),),
-                      ),
-                      Image.network('${data['images']}',
-                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-
-                          title: Text('ชื่อ: ${data['name']}',style: TextStyle(
-                         fontSize: 20 ),),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0,left: 8.0),
-                        child: ListTile(
-                          title: Text('ที่อยู่: ${data['address']}'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0,left: 8.0),
-                        child: ListTile(
-                          title: Text('เวลา: ${data['time']}'),
-                        ),
-                      ),
-
-                      // เพิ่มข้อมูลอื่น ๆ ตามที่ต้องการ
-                    ],
-                  ),
-                ),
-              ],
-              );
-            },
-          );
-        },
-      );
-    }
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        bottomNavigationBar: SalomonBottomBar(
-          currentIndex: _currentIndex,
-          onTap: (i) => setState(() => _currentIndex = i),
-          items: [
-
-            /// Home
-            SalomonBottomBarItem(
-              icon: Icon(Icons.search),
-              title: Text(''),
-              selectedColor: Colors.black,
-            ),
-
-            /// Likes
-            SalomonBottomBarItem(
-              icon: Icon(Icons.qr_code),
-              title: Text(""),
-              selectedColor: Colors.pink,
-            ),
-
-            /// Search
-            SalomonBottomBarItem(
-              icon: Icon(Icons.wallet),
-              title: Text(""),
-              selectedColor: Colors.orange,
-            ),
-
-            /// Profile
-            SalomonBottomBarItem(
-              icon: Icon(Icons.person_2_rounded),
-              title: Text(""),
-              selectedColor: Colors.teal,
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Stack(children: [
-          GoogleMap(
-          initialCameraPosition: CameraPosition(
-          target: _initialCameraPosition,
-            zoom: 14,
-          ),
-          onMapCreated: (controller) {
-            setState(() {
-              _mapController = controller;
-            });
-          },
-          markers: Set<Marker>.from(_markers),
-        ),
-            if (_bottomSheetVisible)
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  color: Colors.white,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('ข้อมูลจาก Firestore'),
-                      // ใส่โค้ดเพื่อแสดงข้อมูลจาก Firestore ของ _selectedMarkerId ที่เลือก
-                    ],
-                  ),
-                ),
-              ),
-            Column(
-              children: [
-                Positioned(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 10.0, left: 20, right: 20),
-                      child: seachBar(),
-                    )
-                ),
-              ],
-            ),
-          ]),
-        ),
-      );
-
-    }
-    Widget seachBar() {
-      return AnimSearchBar(
-        width: 400,
-        textController: searchController,
-        onSuffixTap: () {
-          setState(() {
-            searchController.clear();
-          });
-        }, onSubmitted: (query) {
-        _searchLocation(searchController.text);
-      },
-      );
-    }
-// ค้นหาหมุดจาก Firestore
-  void _searchLocation(String query) {
-    FirebaseFirestore.instance
-        .collection('locations')
-        .where('name', arrayContains: )
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      if (querySnapshot.docs.isNotEmpty) {
-        setState(() {
-          _markers.clear();
-          querySnapshot.docs.forEach((doc) {
-            var location = doc.data() as Map<String, dynamic>;
-            var marker = Marker(
-              markerId: MarkerId(doc.id),
-              position: LatLng(location!['latitude'], location!['longitude']),
-              infoWindow: InfoWindow(
-                title: location['name'],
-              ),
-            );
-            _markers.add(marker);
-          });
-        });
-        // ขยายแผนที่เพื่อแสดงหมุด
-        _mapController?.animateCamera(CameraUpdate.newLatLngBounds(
-          _boundsFromLatLngList(_markers.map((m) => m.position).toList()),
-          50.0,
-        ));
-      } else {
-        // ไม่พบข้อมูล
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('ไม่พบข้อมูลที่ค้นหา'),
-          ),
+  // ดึงข้อมูลหมุดจาก Firestore และสร้าง Marker
+  Future<void> _loadMarkersFromFirestore() async {
+    final markers =
+        await FirebaseFirestore.instance.collection('locations').get();
+    setState(() {
+      _markers = markers.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        final LatLng position = LatLng(data['latitude'], data['longitude']);
+        return Marker(
+          markerId: MarkerId(doc.id),
+          position: position,
+          onTap: () => _showMarkerDetails(doc.id),
         );
-      }
+      }).toList();
     });
   }
+
+  void _showMarkerDetails(String markerId) {
+    setState(() {
+      _selectedMarkerId = markerId;
+    });
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return FutureBuilder<DocumentSnapshot>(
+          future: FirebaseFirestore.instance.collection('locations').doc(_selectedMarkerId).get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text('เกิดข้อผิดพลาดในการดึงข้อมูล');
+            }
+            final data = snapshot.data!.data() as Map<String, dynamic>;
+            return ListView(
+              shrinkWrap: true,
+              children: [
+                Image.network('${data['images']}'),
+                ListTile(
+                  title: Text('ชื่อ: ${data['name']}'),
+                ),
+                ListTile(
+                  title: Text('ที่อยู่: ${data['address']}'),
+                ),
+                ListTile(
+                  title: Text('เวลา: ${data['time']}'),
+                ),
+                // เพิ่มข้อมูลอื่น ๆ ตามที่ต้องการ
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          /// Home
+          SalomonBottomBarItem(
+            icon: Icon(Icons.search),
+            title: Text(''),
+            selectedColor: Colors.black,
+          ),
+
+          /// Likes
+          SalomonBottomBarItem(
+            icon: Icon(Icons.qr_code),
+            title: Text(""),
+            selectedColor: Colors.pink,
+          ),
+
+          /// Search
+          SalomonBottomBarItem(
+            icon: Icon(Icons.wallet),
+            title: Text(""),
+            selectedColor: Colors.orange,
+          ),
+
+          /// Profile
+          SalomonBottomBarItem(
+            icon: Icon(Icons.person_2_rounded),
+            title: Text(""),
+            selectedColor: Colors.teal,
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Stack(children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _initialCameraPosition,
+              zoom: 14,
+            ),
+            onMapCreated: (controller) {
+              setState(() {
+                _mapController = controller;
+              });
+            },
+            markers: Set<Marker>.from(_markers),
+          ),
+          if (_bottomSheetVisible)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: _buildSearchResults(),
+            ),
+          Column(
+            children: [
+              Positioned(
+                  child: Padding(
+                padding: const EdgeInsets.only(top: 10.0, left: 20, right: 20),
+                child: seachBar(),
+              )),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget seachBar() {
+    return TextField(
+      controller: _searchController,
+      onChanged: (query) => _performSearch(query),
+      decoration: InputDecoration(
+        labelText: 'ค้นหา',
+        prefixIcon: Icon(Icons.search),
+      ),
+    );
+  }
+
+  void _performSearch(String query) {
+    FirebaseFirestore.instance
+        .collection('locations')
+        .where('name', isGreaterThanOrEqualTo: query)
+        .where('name', isLessThan: query + 'z')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      setState(() {
+        _searchResults = querySnapshot.docs;
+        _bottomSheetVisible = true; // ทำให้ BottomSheet แสดงผล
+      });
+    });
+  }
+
+
 
   // คำนวณขอบเขตของหมุดทั้งหมด
   LatLngBounds _boundsFromLatLngList(List<LatLng> list) {
@@ -265,7 +204,31 @@ shape: RoundedRectangleBorder(
       northeast: LatLng(north, east),
     );
   }
+
+  void _selectSearchResult(DocumentSnapshot result) {
+    final data = result.data() as Map<String, dynamic>;
+    final LatLng position = LatLng(data['latitude'], data['longitude']);
+
+    // ย้ายกล้องให้ชี้ที่ตำแหน่งของหมุด
+    _mapController!.animateCamera(CameraUpdate.newLatLng(position));
+
+    // แสดงข้อมูลของหมุดที่เลือกผ่าน BottomSheet
+    _showMarkerDetails(result.id);
+  }
+
+  Widget _buildSearchResults() {
+    return Container(
+      color: Colors.white,
+      child: ListView.builder(
+        itemCount: _searchResults.length,
+        itemBuilder: (context, index) {
+          final data = _searchResults[index].data() as Map<String, dynamic>;
+          return ListTile(
+            title: Text(data['name']),
+            onTap: () => _selectSearchResult(_searchResults[index]),
+          );
+        },
+      ),
+    );
+  }
 }
-
-
-
