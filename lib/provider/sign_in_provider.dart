7e5ee5a66a8910b2bca164767ba2cb2bd6edc1ff
Chayut Yuttapornpong <yoshi9968@gmail.com>
2023-09-29 11:28:15 +0700

    Fixed

--- lib/provider/sign_in_provider.dart
+++ lib/provider/sign_in_provider.dart
@@ -248,71 +248,66 @@ class SignInProvide extends ChangeNotifier {
       print("NEW USER");
       return false;
     }
-
-    // Sigin with phone
-    void signInWithPhone(BuildContext context, String phoneNumber) async {
-      try {
-        await _firebaseAuth.verifyPhoneNumber(
-            phoneNumber: phoneNumber,
-            verificationCompleted:
-                (PhoneAuthCredential phoneAuthCredential) async {
-              await _firebaseAuth.signInWithCredential(phoneAuthCredential);
-            },
-            verificationFailed: (error) {
-              throw Exception(error.message);
-            },
-            codeSent: (verificationId, forceResendingToken) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      Forget_otp(verificationId: verificationId),
-                ),
-              );
-            },
-            codeAutoRetrievalTimeout: (verificationId) {});
-      } on FirebaseAuthException catch (e) {
-        showSnackBar(context, e.message.toString());
-      }
-    }
-
-// verifyOtp
-    void verifyOtp({
-      required BuildContext context,
-      required String verificationId,
-      required String userOtp,
-      required Function onSuccess,
-    }) async {
-      _isLoading = true;
-      notifyListeners();
-
-      try {
-        PhoneAuthCredential creds = PhoneAuthProvider.credential(
-            verificationId: verificationId, smsCode: userOtp);
-        User? user = (await _firebaseAuth.signInWithCredential(creds)).user!;
-        if (user != null) {
-          //carry our logic
-
-          onSuccess();
-        }
-        _uid = user.uid;
-        _isLoading = false;
-        notifyListeners();
-      } on FirebaseAuthException catch (e) {
-        showSnackBar(context, e.message.toString());
-        _isLoading = false;
-        notifyListeners();
-      }
-    }
-
-// Database opertaions
   }
 
-  void verifyOtp(
-      {required BuildContext context,
-      required String verificationId,
-      required String userOtp,
-      required Null Function() onSuccess}) {}
+  // Sigin with phone
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try {
+      await _firebaseAuth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted:
+              (PhoneAuthCredential phoneAuthCredential) async {
+            await _firebaseAuth.signInWithCredential(phoneAuthCredential);
+          },
+          verificationFailed: (error) {
+            throw Exception(error.message);
+          },
+          codeSent: (verificationId, forceResendingToken) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    Forget_otp(verificationId: verificationId),
+              ),
+            );
+          },
+          codeAutoRetrievalTimeout: (verificationId) {});
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
+    }
+  }
 
-  void signInWithPhone(BuildContext context, String s) {}
+// verifyOtp
+  void verifyOtp({
+    required BuildContext context,
+    required String verificationId,
+    required String userOtp,
+    required Function onSuccess,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      PhoneAuthCredential creds = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: userOtp);
+      User? user = (await _firebaseAuth.signInWithCredential(creds)).user!;
+      if (user != null) {
+        //carry our logic
+
+        onSuccess();
+      }
+      _uid = user.uid;
+      _isLoading = false;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+// Database opertaions
+
+
+
 }
